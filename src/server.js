bbe84f92cf386d2064dfa51ee017158875f0119e
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-02-12 13:19:10 +0530

    First Commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  try {
+    return res.status(200).send({ message: "Hello Welcome" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
+  }
+});
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  try {
+    console.log(`listening on port ${port}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
